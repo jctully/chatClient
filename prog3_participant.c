4a260f6a11d32e484ffa2a9595ba144b80857843
Joseph Tully <tullyj2@wwu.edu>
2018-12-03 16:43:18 -0800

    reorder code, select working with 1 participant

--- prog3_participant.c
+++ prog3_participant.c
@@ -28,141 +28,141 @@
 */
 
 void fullRead(int sd, char* buffer, int length) {
-	int m;
-	int n =  recv(sd, buffer, length, 0);
-	if (n<0) {
-		perror("recv");
-		exit(EXIT_FAILURE);
-	}
-	while (n < length) {
-		m =  recv(sd, buffer, length - n, 0);
-		if (m<0) {
-			perror("recv");
-			exit(EXIT_FAILURE);
-		}
-		n += m;
-	}
-	buffer[length] = '\0';
+  int m;
+  int n =  recv(sd, buffer, length, 0);
+  if (n<0) {
+    perror("recv");
+    exit(EXIT_FAILURE);
+  }
+  while (n < length) {
+    m =  recv(sd, buffer, length - n, 0);
+    if (m<0) {
+      perror("recv");
+      exit(EXIT_FAILURE);
+    }
+    n += m;
+  }
+  buffer[length] = '\0';
 }
 
 int main( int argc, char **argv) {
-	struct hostent *ptrh; /* pointer to a host table entry */
-	struct protoent *ptrp; /* pointer to a protocol table entry */
-	struct sockaddr_in sad; /* structure to hold an IP address */
-	int sd; /* socket descriptor */
-	int port; /* protocol port number */
-	char *host; /* pointer to host name */
-	int n; /* number of characters read */
-	char buf[1000]; /* buffer for data from the server */
-	uint16_t nameLen;
-    uint16_t messageLen;
-	char username[10], message[255];
-	char letter;
-	int on = 1;
-	int sock;
-	int rv;
+  struct hostent *ptrh; /* pointer to a host table entry */
+  struct protoent *ptrp; /* pointer to a protocol table entry */
+  struct sockaddr_in sad; /* structure to hold an IP address */
+  int sd; /* socket descriptor */
+  int port; /* protocol port number */
+  char *host; /* pointer to host name */
+  int n; /* number of characters read */
+  char buf[1000]; /* buffer for data from the server */
+  uint16_t nameLen;
+  uint16_t messageLen;
+  char username[10], message[255];
+  char letter;
+  int on = 1;
+  int sock;
+  int rv;
 
-	fd_set readfds;
-	FD_ZERO(&readfds);
+  fd_set readfds;
+  FD_ZERO(&readfds);
 
-	memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
-	sad.sin_family = AF_INET; /* set family to Internet */
+  memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
+  sad.sin_family = AF_INET; /* set family to Internet */
 
-	if( argc != 3 ) {
-		fprintf(stderr,"Error: Wrong number of arguments\n");
-		fprintf(stderr,"usage:\n");
-		fprintf(stderr,"./client server_address server_port\n");
-		exit(EXIT_FAILURE);
-	}
+  if( argc != 3 ) {
+    fprintf(stderr,"Error: Wrong number of arguments\n");
+    fprintf(stderr,"usage:\n");
+    fprintf(stderr,"./client server_address server_port\n");
+    exit(EXIT_FAILURE);
+  }
 
-	port = atoi(argv[2]); /* convert to binary */
-	if (port > 0) /* test for legal value */
-		sad.sin_port = htons((u_short)port);
-	else {
-		fprintf(stderr,"Error: bad port number %s\n",argv[2]);
-		exit(EXIT_FAILURE);
-	}
+  port = atoi(argv[2]); /* convert to binary */
+  if (port > 0) /* test for legal value */
+  sad.sin_port = htons((u_short)port);
+  else {
+    fprintf(stderr,"Error: bad port number %s\n",argv[2]);
+    exit(EXIT_FAILURE);
+  }
 
-	host = argv[1]; /* if host argument specified */
+  host = argv[1]; /* if host argument specified */
 
-	/* Convert host name to equivalent IP address and copy to sad. */
-	ptrh = gethostbyname(host);
-	if ( ptrh == NULL ) {
-		fprintf(stderr,"Error: Invalid host: %s\n", host);
-		exit(EXIT_FAILURE);
-	}
+  /* Convert host name to equivalent IP address and copy to sad. */
+  ptrh = gethostbyname(host);
+  if ( ptrh == NULL ) {
+    fprintf(stderr,"Error: Invalid host: %s\n", host);
+    exit(EXIT_FAILURE);
+  }
 
-	memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
+  memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
 
-	/* Map TCP transport protocol name to protocol number. */
-	if ( ((long int)(ptrp = getprotobyname("tcp"))) == 0) {
-		fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
-		exit(EXIT_FAILURE);
-	}
+  /* Map TCP transport protocol name to protocol number. */
+  if ( ((long int)(ptrp = getprotobyname("tcp"))) == 0) {
+    fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
+    exit(EXIT_FAILURE);
+  }
 
-	/* Create a socket. */
-	sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
-	if (sd < 0) {
-		fprintf(stderr, "Error: Socket creation failed\n");
-		exit(EXIT_FAILURE);
-	}
-	rv = ioctl(sd, FIONBIO, (char *)&on);
-	if (rv < 0)
-	{
-	  perror("ioctl() failed");
-	  close(sd);
-	  exit(EXIT_FAILURE);
-	}
+  /* Create a socket. */
+  sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
+  if (sd < 0) {
+    fprintf(stderr, "Error: Socket creation failed\n");
+    exit(EXIT_FAILURE);
+  }
+  rv = ioctl(sd, FIONBIO, (char *)&on);
+  if (rv < 0)
+  {
+    perror("ioctl() failed");
+    close(sd);
+    exit(EXIT_FAILURE);
+  }
 
-	FD_SET(sd, &readfds);
-	sock = sd+1;
+  FD_SET(sd, &readfds);
+  sock = sd+1;
 
-	/* TODO: Connect the socket to the specified server. You have to pass correct parameters to the connect function.*/
-	if (connect(sd, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
-		if (errno != EINPROGRESS) {
-			perror("connect");
-			exit(EXIT_FAILURE);
-		}
-	}
+  /* TODO: Connect the socket to the specified server. You have to pass correct parameters to the connect function.*/
+  if (connect(sd, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
+    if (errno != EINPROGRESS) {
+      perror("connect");
+      exit(EXIT_FAILURE);
+    }
+  }
 
-	/* Repeatedly read data from socket and write to user's screen. */
-	rv = select(sock, &readfds, NULL, NULL, NULL);
-	if (rv == -1) {
-			perror("select"); // error occurred in select()
-	}
+  /* Repeatedly read data from socket and write to user's screen. */
+  rv = select(sock, &readfds, NULL, NULL, NULL);
+  if (rv == -1) {
+    perror("select"); // error occurred in select()
+  }
 
-    // Check if theres room for another connection
-		printf("waiting for first letter\n");
-	recv(sd, &letter, sizeof(letter), 0);
-	if (letter == 'Y') {
-		//prompt input
-		printf("Enter your username: ");
-		fgets(username, 10, stdin);
-		printf("\n");
-		//validate name, timer
-		nameLen = strlen(username);
-		printf("len = %d, name = .%s.\n", nameLen, username);
-		//nameLen = htons(nameLen);
-		send(sd, &nameLen, sizeof(nameLen), 0);
-		send(sd, username, nameLen, 0);
-		printf("username sent\n");
+  // Check if theres room for another connection
+  printf("waiting for first letter\n");
+  recv(sd, &letter, sizeof(letter), 0);
+  if (letter == 'Y') {
+    //prompt input
+    printf("Enter your username: ");
+    fgets(username, 10, stdin);
+    printf("\n");
+    //validate name, timer
+    nameLen = strlen(username);
+    printf("len = %d, name = .%s.\n", nameLen, username);
+    //nameLen = htons(nameLen);
+    send(sd, &nameLen, sizeof(nameLen), 0);
+    send(sd, username, nameLen, 0);
+    printf("username sent\n");
 
-        recv(sd, &letter, sizeof(letter), 0);
-        printf("Read letter: %c\n", letter);
-        if(letter == 'Y') {
-            while(1) {
-                printf("Enter message: ");
-                fgets(message, 255, stdin);
-                messageLen = strlen(message);
-                send(sd, &messageLen, sizeof(messageLen), 0);
-								send(sd, message, messageLen, 0);
-                printf("message sent: \"%s\"\n", message);
-            }
-        }
-	}
+    recv(sd, &letter, sizeof(letter), 0);
+    printf("Read letter: %c\n", letter);
+    if(letter == 'Y') {
+      while(1) {
+        printf("Enter message: ");
+        fgets(message, 255, stdin);
+        messageLen = strlen(message);
+        send(sd, &messageLen, sizeof(messageLen), 0);
+        send(sd, message, messageLen, 0);
+        printf("message sent: \"%s\"\n", message);
+      }
+    }
+  }
 
 
-	close(sd);
+  close(sd);
 
-	exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
--- prog3_server.c
+++ prog3_server.c
@@ -37,190 +37,199 @@ int numParticipants = 0; /* counts client connections */
 */
 
 char* fullRead(int sd, char* message) {
-	uint16_t messageLen = 0;
+  uint16_t messageLen = 0;
 
-	recv(sd, &messageLen, sizeof(messageLen), MSG_WAITALL);
-	//printf("converetd len %d\n", messageLen);
-	recv(sd, message, messageLen, MSG_WAITALL);
-	//printf("message: \"%s\"\n", message);
-	message[messageLen-1] = '\0';
+  recv(sd, &messageLen, sizeof(messageLen), 0);
+  //printf("converetd len %d\n", messageLen);
+  recv(sd, message, messageLen, 0);
+  //printf("message: \"%s\"\n", message);
+  message[messageLen-1] = '\0';
 
 }
 
 int resetFDSet(fd_set *readfds, int sd[255], int sdl) {
-	printf("inside reset fds function\n");
-	FD_ZERO(readfds);
-	FD_SET(sdl, readfds);
-	printf("here\n");
-	int maxsd = -1;
-	for (int i=0; i<255; i++) {
-		if (sd[i] != -1) {
-			printf("sd %d in use\n", i);
-			FD_SET(sd[i], readfds);
-			if (sd[i] > maxsd) {
-				maxsd = sd[i];
-			}
-		}
-	}
-	return ++maxsd;
+  printf("inside reset fds function\n");
+  FD_ZERO(readfds);
+  FD_SET(sdl, readfds);
+  int maxsd = sdl;
+  for (int i=0; i<255; i++) {
+    if (sd[i] != -1) {
+      printf("sd %d in use\n", i);
+      FD_SET(sd[i], readfds);
+      if (sd[i] > maxsd) {
+        maxsd = sd[i];
+      }
+    }
+  }
+  return ++maxsd;
 }
 
 int main(int argc, char **argv) {
-	struct protoent *ptrp; /* pointer to a protocol table entry */
-	struct sockaddr_in sad; /* structure to hold server's address */
-	struct sockaddr_in cad; /* structure to hold client's address */
-	int sd, sdt, sd2[255]; /* socket descriptors */
-	int port; /* protocol port number */
-	int alen; /* length of address */
-	int optval = 1; /* boolean value when we set socket option */
-	char buf[1000]; /* buffer for string the server sends */
-	int MAX_PARTICIPANTS = 255;
-	char n = 'N', y = 'Y', t='T', invalid = 'I';
-	char username[10];
+  struct protoent *ptrp; /* pointer to a protocol table entry */
+  struct sockaddr_in sad; /* structure to hold server's address */
+  struct sockaddr_in cad; /* structure to hold client's address */
+  int sd, sdt, sd2[255]; /* socket descriptors */
+  int port; /* protocol port number */
+  int alen; /* length of address */
+  int optval = 1; /* boolean value when we set socket option */
+  char buf[1000]; /* buffer for string the server sends */
+  int MAX_PARTICIPANTS = 255;
+  char n = 'N', y = 'Y', t='T', invalid = 'I';
+  char username[10];
   char message[255];
-	uint16_t nameLen, messageLen;
-	int rv, sock;
-	int on = 1;
-	struct Trie *activeUsers;
+  uint16_t nameLen, messageLen;
+  int rv, sock;
+  int on = 1;
+  struct Trie *activeUsers;;;
 
-	for (int i=0; i<255; i++) {
-		sd2[i] = -1;
-	}
+  for (int i=0; i<255; i++) {
+    sd2[i] = -1;
+  }
 
-	fd_set readfds;
-	FD_ZERO(&readfds);
+  fd_set readfds;
+  FD_ZERO(&readfds);
 
-	activeUsers = getNewTrieNode();
+  activeUsers = getNewTrieNode();
 
-	if( argc != 3 ) {
-		fprintf(stderr,"Error: Wrong number of arguments\n");
-		fprintf(stderr,"usage:\n");
-		fprintf(stderr,"./server participant_port observer_port\n");
-		exit(EXIT_FAILURE);
-	}
+  if( argc != 3 ) {
+    fprintf(stderr,"Error: Wrong number of arguments\n");
+    fprintf(stderr,"usage:\n");
+    fprintf(stderr,"./server participant_port observer_port\n");
+    exit(EXIT_FAILURE);
+  }
 
-	int pPort = atoi(argv[1]);
-	int oPort = atoi(argv[2]);
+  int pPort = atoi(argv[1]);
+  int oPort = atoi(argv[2]);
 
-	memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
+  memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
 
-	//TODO: Set socket family to AF_INET
-	sad.sin_family = AF_INET;
+  //TODO: Set socket family to AF_INET
+  sad.sin_family = AF_INET;
 
-	//TODO: Set local IP address to listen to all IP addresses this server can assume. You can do it by using INADDR_ANY
-	sad.sin_addr.s_addr = INADDR_ANY;
-	port = atoi(argv[1]); /* convert argument to binary */
-	if (port > 0) { /* test for illegal value */
-		//TODO: set port number. The data type is u_short
-		sad.sin_port = htons(port);
-	} else { /* print error message and exit */
-		fprintf(stderr,"Error: Bad port number %s\n",argv[1]);
-		exit(EXIT_FAILURE);
-	}
+  //TODO: Set local IP address to listen to all IP addresses this server can assume. You can do it by using INADDR_ANY
+  sad.sin_addr.s_addr = INADDR_ANY;
+  port = atoi(argv[1]); /* convert argument to binary */
+  if (port > 0) { /* test for illegal value */
+    //TODO: set port number. The data type is u_short
+    sad.sin_port = htons(port);
+  } else { /* print error message and exit */
+    fprintf(stderr,"Error: Bad port number %s\n",argv[1]);
+    exit(EXIT_FAILURE);
+  }
 
-	/* Map TCP transport protocol name to protocol number */
-	if ( ((long int)(ptrp = getprotobyname("tcp"))) == 0) {
-		fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
-		exit(EXIT_FAILURE);
-	}
+  /* Map TCP transport protocol name to protocol number */
+  if ( ((long int)(ptrp = getprotobyname("tcp"))) == 0) {
+    fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
+    exit(EXIT_FAILURE);
+  }
 
-	/* TODO: Create a socket with AF_INET as domain, protocol type as SOCK_STREAM, and protocol as ptrp->p_proto. This call returns a socket descriptor named sd. */
-	sd = socket(AF_INET, SOCK_STREAM, ptrp->p_proto);
-	if (sd < 0) {
-		fprintf(stderr, "Error: Socket creation failed\n");
-		exit(EXIT_FAILURE);
-	}
+  /* TODO: Create a socket with AF_INET as domain, protocol type as SOCK_STREAM, and protocol as ptrp->p_proto. This call returns a socket descriptor named sd. */
+  sd = socket(AF_INET, SOCK_STREAM, ptrp->p_proto);
+  if (sd < 0) {
+    fprintf(stderr, "Error: Socket creation failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-	/* Allow reuse of port - avoid "Bind failed" issues */
-	if( setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0 ) {
-		fprintf(stderr, "Error Setting socket option failed\n");
-		exit(EXIT_FAILURE);
-	}
+  /* Allow reuse of port - avoid "Bind failed" issues */
+  if( setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0 ) {
+    fprintf(stderr, "Error Setting socket option failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-	/* TODO: Bind a local address to the socket. For this, you need to pass correct parameters to the bind function. */
-	if (bind(sd, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
-		fprintf(stderr,"Error: Bind failed\n");
-		exit(EXIT_FAILURE);
-	}
+  /* TODO: Bind a local address to the socket. For this, you need to pass correct parameters to the bind function. */
+  if (bind(sd, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
+    fprintf(stderr,"Error: Bind failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-	/* Makes sd (and children) nonblocking sockets*/
-rv = ioctl(sd, FIONBIO, (char *)&on);
-if (rv < 0)
-{
-	perror("ioctl() failed");
-	close(sd);
-	exit(EXIT_FAILURE);
-}
+  /* Makes sd (and children) nonblocking sockets*/
+  rv = ioctl(sd, FIONBIO, (char *)&on);
+  if (rv < 0)
+  {
+    perror("ioctl() failed");
+    close(sd);
+    exit(EXIT_FAILURE);
+  }
 
-	/* TODO: Specify size of request queue. Listen take 2 parameters -- socket descriptor and QLEN, which has been set at the top of this code. */
-	if (listen(sd, QLEN) < 0) {
-		fprintf(stderr,"Error: Listen failed\n");
-		exit(EXIT_FAILURE);
-	}
+  /* TODO: Specify size of request queue. Listen take 2 parameters -- socket descriptor and QLEN, which has been set at the top of this code. */
+  if (listen(sd, QLEN) < 0) {
+    fprintf(stderr,"Error: Listen failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-	FD_SET(sd, &readfds);
-	sock = sd+1;
-	/* Main server loop - accept and handle requests */
-	while (1) {
-		alen = sizeof(cad);
-		rv = select(sock, &readfds, NULL, NULL, NULL);
-		if (rv == -1) {
-				perror("select"); // error occurred in select()
-		}
-    // Accept client connections
-		if ((sd2[0] = accept(sd, (struct sockaddr *)&cad, &alen)) < 0) {
-			perror("accept");
-			exit(EXIT_FAILURE);
-		}
+  int i;
 
-		numParticipants++;
-		if (numParticipants > MAX_PARTICIPANTS) {
-			send(sd2[0], &n, 1, 0);
-			close(sd2[0]);
-			numParticipants--;
-		}
+  /* Main server loop - accept and handle requests */
+  while (1) {
+    for (i=0; i<255; i++) {
+      if (sd2[i]==-1) {
+        break;
+      }
+    }
+    printf("next open ind = %d\n", i);
+    alen = sizeof(cad);
+    sock = resetFDSet(&readfds, sd2, sd);
+    printf("sock = %d\n", sock);
+    rv = select(sock, &readfds, NULL, NULL, NULL);
+    if (rv == -1) {
+      perror("select"); // error occurred in select()
+      exit(EXIT_FAILURE);
+    }
+    printf("after select\n");
+    // Accept client connections }
 
-    // Check for valid active participant
-		//printf("sending first letter\n");
-		send(sd2[0], &y, 1, 0);
-		int validName = 0;
-		while (validName == 0) {
-			//fullRead(sd2, &nameLen, sizeof(nameLen));
-			fullRead(sd2[0], usock, &readfdssername);
-			printf("name = .%s.\n", username);
+    if (FD_ISSET(sd, &readfds)) {
+      if ((sd2[i] = accept(sd, (struct sockaddr *)&cad, &alen)) < 0) {
+         perror("accept");
+         exit(EXIT_FAILURE);
+      }
+      numParticipants++;
+      if (numParticipants > MAX_PARTICIPANTS) {
+        send(sd2[0], &n, 1, 0);
+        close(sd2[0]);
+        sd2[0] = -1;
+        numParticipants--;
+      }
 
-			//validate username
+      // Check for valid active participant
+      //printf("sending first letter\n");
+      send(sd2[0], &y, 1, 0);
+      int validName = 0;
+      while (validName == 0) {
+        //fullRead(sd2, &nameLen, sizeof(nameLen));
+        fullRead(sd2[0], username);
+        printf("name = .%s.\n", username);
 
-			//rv = search(activeUsers, username);
-      // Validate username
-      rv = 0;
+        //validate username
 
-			if (rv == 1) {
+        //rv = search(activeUsers, username);
+        // Validate username
+        rv = 0;
 
-				send(sd2[0], &t, sizeof(t), 0);
-			}
-			else {
+        if (rv == 1) {
 
-				validName = 1;
-				send(sd2[0], &y, sizeof(y), 0);
-			}
-		}	//use select here?
-    		while(1) {
-					printf("sd2[0] = %d\n", sd2[0]);
-					sock = resetFDSet(&readfds, sd2, sd);
-					printf("fds reset, sock = %d\n", sock);
-					rv = select(sock, &readfds, NULL, NULL, NULL);
-					if (rv == -1) {
-							perror("select"); // error occurred in select()
-					}
-            // active participant messages
-          printf("Waiting to recv\n");
-					if (FD_ISSET(sd2[0], &readfds)) {
-          	fullRead(sd2[0], message);
-          	printf("terminated message: .%s.\n", message);
-					}
+          send(sd2[0], &t, sizeof(t), 0);
         }
+        else {
 
-		close(sd2[0]);
-	}
+          validName = 1;
+          send(sd2[0], &y, sizeof(y), 0);
+        }
+      }
+
+   }
+
+    printf("sd2[0] = %d\n", sd2[0]);
+
+    // active participant messages
+    printf("Waiting to recv\n");
+    if (FD_ISSET(sd2[0], &readfds)) {
+      printf("sd2[0] set\n");
+      fullRead(sd2[0], message);
+      printf("terminated message: .%s.\n", message);
+    }
+
+  }
+  close(sd2[0]);
+  sd2[0] = -1;
 }
